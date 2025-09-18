e56f47529f86a8ceb38f9f6e0bcd2f50467bdd43
isabellasaiden <isamedinasaiden@hotmail.com>
2025-09-17 20:58:32 -0600

    port

--- app.py
+++ app.py
@@ -1,32 +1,26 @@
 from flask import Flask
 import os
 from dotenv import load_dotenv
-from config.db import init_db
+from config.db import init_db, mysql
 from flask_jwt_extended import JWTManager
 
-# Rutas (blueprints)
 from routes.tareas import tareas_bp
 from routes.usuarios import usuarios_bp
 
-# Cargar variables de entorno
 load_dotenv()
 
 def create_app():
     app = Flask(__name__)
     init_db(app)
 
-    app.config['JWT_SECRET_KEY'] = os.getenv('JWT_SECRET', 'secret-key')
-    JWTManager(app)
+    app.config['JWT_SECRET_KEY'] = os.getenv('JWT_SECRET')
+    jwt = JWTManager(app)
 
-    # Registrar rutas
+    # Registrar blueprints
     app.register_blueprint(tareas_bp, url_prefix="/tareas")
-    app.register_blueprint(usuarios_bp, url_prefix="/usuarios")
+    app.register_blueprint(usuarios_bp, url_prefix='/usuarios')
 
     return app
 
-# Instancia de la app
+# esta es la variable que gunicorn usa
 app = create_app()
-
-if __name__ == '__main__':
-    port = int(os.getenv('PORT', 8080))
-    app.run(host='0.0.0.0', port=port, debug=True)
